f26bafb1c8d04fb36e5897e68a152e5d68dcb315
Dmitry Pertsev <disay2040@yandex.ru>
2025-01-25 14:53:34 +0300

    1st commit, worked project

--- src/services/store.ts
+++ src/services/store.ts
@@ -1,15 +1,36 @@
 import { configureStore } from '@reduxjs/toolkit';
-
+import { combineReducers } from '@reduxjs/toolkit';
+import ingredientsReducer from './ingredientSlice';
+import constructorReducer from './constructorSlice';
+import feedsSlice from './feedsSlice';
 import {
   TypedUseSelectorHook,
   useDispatch as dispatchHook,
   useSelector as selectorHook
 } from 'react-redux';
 
-const rootReducer = () => {}; // Заменить на импорт настоящего редьюсера
+import { Middleware } from '@reduxjs/toolkit';
+import orderSlice from './orderSlice';
+
+import userSlice from './userSlice';
+
+const loggerMiddleware: Middleware = (store) => (next) => (action) => {
+  console.log('Next state:', store.getState());
+  return next(action);
+};
+
+const rootReducer = combineReducers({
+  ingredientsSlice: ingredientsReducer,
+  constructorSlice: constructorReducer,
+  feedsSlice: feedsSlice,
+  orderSlice: orderSlice,
+  userSlice: userSlice
+});
 
 const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(loggerMiddleware),
   devTools: process.env.NODE_ENV !== 'production'
 });
 
